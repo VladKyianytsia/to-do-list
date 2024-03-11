1a7ec3d07ea5e4080a734cfb3a48ba1390eb7200
Vlad Kyianytsia <kiyanitsa2002021@gmail.com>
2024-03-11 10:44:22 +0200

    added Task & Tag models and migrated them to db

--- to_do_app/models.py
+++ to_do_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=65)
+
+
+class Task(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateField(blank=True, null=True)
+    done = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    class Meta:
+        ordering = ("done",)

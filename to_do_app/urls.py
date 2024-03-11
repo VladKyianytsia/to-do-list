39c81e2ea717a2dd40823c09d0ba39ffffccf5b2
Vlad Kyianytsia <kiyanitsa2002021@gmail.com>
2024-03-11 11:21:34 +0200

    added task-list url and TaskListView for it

--- to_do_app/urls.py
+++ to_do_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
+from to_do_app.views import TaskListView
 
 app_name = "to_do_app"
 
 urlpatterns = [
-
+    path("", TaskListView.as_view(), name="task-list")
 ]
--- to_do_app/views.py
+++ to_do_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from to_do_app.models import Task
+
+
+class TaskListView(generic.ListView):
+    model = Task
